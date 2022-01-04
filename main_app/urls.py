b529c20b4128cf5c84a56df5df784aa1eac3c905
Darby Benfield <thinxing@hotmail.com>
2022-01-03 23:35:05 -0700

    CRUD URLS COMPLETE

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'), 
   path('meds/', views.meds_index, name="meds_index"),
+  path('meds/<int:med_id>/', views.meds_detail, name='meds_detail'),
+  # path('meds/create/', views.MedCreate.as_view(), name='meds_create'),
+  # path('meds/<int:pk>/update/', views.MedUpdate.as_view(), name='meds_update'),
+  # path('meds/<int:pk>/delete/', views.MedDelete.as_view(), name='meds_delete'),
   ]
