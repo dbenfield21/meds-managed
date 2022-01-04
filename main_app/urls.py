41426b50b92a306c3fa5ef8e4d075a3662bbfc80
Darby Benfield <thinxing@hotmail.com>
2022-01-04 16:17:56 -0700

    login

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'), 
   path('meds/', views.meds_index, name="meds_index"),
   path('meds/<int:med_id>/', views.meds_detail, name='meds_detail'),
   path('meds/create/', views.MedCreate.as_view(), name='meds_create'),
   path('meds/<int:pk>/update/', views.MedUpdate.as_view(), name='meds_update'),
   path('meds/<int:pk>/delete/', views.MedDelete.as_view(), name='meds_delete'),
+  path('accounts/signup/', views.signup, name='signup'),
   ]
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -4,13 +4,16 @@ from django.db.models import fields
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Med 
 
 # Create your views here.
 
 
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
@@ -28,6 +31,9 @@ class MedCreate (CreateView):
   model = Med
   fields = ['name', 'dose', 'notes', 'taken']
   success_url = '/meds/'
+  def form_valid(self, form):
+    form.instance.user = self.request.user  
+    return super().form_valid(form)
 
 class MedUpdate (UpdateView):
   model = Med
@@ -38,4 +44,21 @@ class MedDelete (DeleteView):
   model = Med
   success_url = '/meds/'
   
-
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in
+      login(request, user)
+      return redirect('meds_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
